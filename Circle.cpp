f567ba03c11264c115104e167c0e8adc53e37139
Moksh Bhavsar <55453620+moksh-bhavsar@users.noreply.github.com>
2020-02-14 09:43:00 -0500

    Add files via upload

--- Circle.cpp
+++ Circle.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
+double Pi = 3.14159265359;
+
 class Circle {
-  private:  
+  private:
     int x,y;      // center of circle
     double r;    // radius of circle
 
   public:
-   
+
     Circle(int =0,int=0, double =1.0);
     void print() const;
     double area() const;
@@ -24,31 +26,36 @@ class Circle {
 Circle::Circle(int x,int y, double r)
 {
   // Initialize the object here
+  this->x = x;
+  this->y = y;
+  this->r = r;
 }
 
 double Circle::area() const {
-  // Return the area of a circle  
+  // Return the area of a circle
+  return (Pi*r*r);
 }
 
 double Circle::operator+(const Circle & c) const
 {
-   // + Return the area of two circles  
+   // + Return the area of two circles
+   return (area() + c.area());
 
 }
 
 Circle & Circle::operator++()
 {
   // Increment the radius by 1
-
+  r = r + 1;
   return *this;
 }
 
 Circle Circle::operator++(int)
 {
   Circle temp = *this;
-  
-  //increment the radius by 1
 
+  //increment the radius by 1
+  r = r + 1;
   return temp;
 }
 
@@ -56,14 +63,18 @@ ostream & operator<<(ostream &out, const Circle &c)
 {
 
   // print a circle in the out stream (the format is the same as print())
+  out << "Circle =(" << c.x << "," << c.y << "," << c.r << ")" << endl;
   return out;
 }
 
 istream & operator>>(istream &in, Circle &c)
 {
- 
- // Read a circle information form the in stream
 
+ // Read a circle information form the in stream
+ cout << "Origin of the circle" << endl;
+ in >> c.x >> c.y;
+ cout << "radius of the circle" << endl;
+ in >> c.r;
   return in;
 }
 
@@ -77,23 +88,23 @@ void Circle::print() const
 
 int main()
 {
-  // Circle a;
-  // Circle b(2,3,3.0);
+  Circle a;
+  Circle b(2,3,3.0);
 
-  // a.print();
-  // b.print();
-  
-  // cout << "The area a + b is: " << a+b << endl;
-  // ++a;
-  // a.print();
+  a.print();
+  b.print();
 
-  // a++;
-  // a.print();
+  cout << "The area a + b is: " << a+b << endl;
+  ++a;
+  a.print();
 
-  // cout << a;
+  a++;
+  a.print();
 
-  // cin >> a;
-  // cout <<a;
+  cout << a;
+
+  cin >> a;
+  cout <<a;
 
   return 0;
 
